7ea8255ce6ec97de597a3ec72feaec2b808db6b0
Dan Bonneville <DanMBonneville@gmail.com>
2024-03-04 08:30:26 -0600

    Fix linting errors

--- src/utils.tsx
+++ src/utils.tsx
@@ -4,7 +4,7 @@ export function isSolved(gameArray: String[]) {
     let cell = gameArray[index];
     let row = getRowFromIndex(index);
     let col = getColFromIndex(index);
-    if ('0' == cell) return false;
+    if ('0' === cell) return false;
     if (getValuesAssociatedToCell(row, col, grid).includes(cell)) return false;
   }
   return true;
@@ -44,8 +44,8 @@ const getRowAndColumnValues = (
 ) => {
   const valuesAssociatedToCell: String[] = [];
   for (let index = 0; index < 9; index++) {
-    if (index != col) valuesAssociatedToCell.push(currentGrid[row][index]);
-    if (index != row) valuesAssociatedToCell.push(currentGrid[index][col]);
+    if (index !== col) valuesAssociatedToCell.push(currentGrid[row][index]);
+    if (index !== row) valuesAssociatedToCell.push(currentGrid[index][col]);
   }
   return valuesAssociatedToCell;
 };
@@ -56,7 +56,7 @@ const getLocalBoxValues = (row: number, col: number, currentGrid: any) => {
   const startingCol = col - (col % 3);
   for (let rowNum = startingRow; rowNum < startingRow + 3; rowNum++) {
     for (let colNum = startingCol; colNum < startingCol + 3; colNum++) {
-      if (rowNum == row && colNum == col) continue;
+      if (rowNum === row && colNum === col) continue;
       valuesToRemove.push(currentGrid[rowNum][colNum]);
     }
   }
